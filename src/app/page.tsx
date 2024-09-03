2a084d08952f41dea2eac4c8c5f2019670a2448b
bruno <brunofraga@prendeluz.com>
2024-09-03 12:43:03 +0200

    feat():loging and middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,48 @@
-import Image from "next/image";
+"use client";
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 export default function Home() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>(''); // Estado para almacenar el mensaje de error
+  const router = useRouter(); // Usar el hook useRouter
+
+
+  const  apiUrl   = process.env.NEXT_PUBLIC_BACKEND_API_URL as string;
+  console.log(apiUrl);
+
+  // Función para manejar el envío del formulario
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement> ) => {
+    event.preventDefault(); // Previene la recarga de la página
+
+    // Realiza la solicitud POST al backend usando axios
+    try {
+      const response = await axios.post(apiUrl+"/login", {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        const token: string = response.data.token;
+        document.cookie = `erp_token=${token}`;
+        setErrorMessage('');
+        router.push('/dashboard');
+        
+        // Aquí puedes manejar lo que sucede después de un login exitoso
+        // Por ejemplo, guardar un token de autenticación, redirigir, etc.
+      } else {
+        console.error('Error al iniciar sesión');
+        // Maneja el error de login, muestra un mensaje, etc.
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+      setErrorMessage('No se ha podido acceder revise sus crendeciales');
+
+      // Maneja errores de red, muestra un mensaje, etc.
+    }
+  };
   return (
     <main className="flex w-full min-h-screen flex-col items-center justify-between p-24">
       <section className="bg-gray-50 dark:bg-gray-900 rounded-lg  ">
@@ -10,17 +52,21 @@ export default function Home() {
           </a>
           <div className="w-full bg-gray-50 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Accede con tus credenciales
+              <h1 className={`text-lg font-bold leading-tight tracking-tight md:text-2xl 
+              ${errorMessage ? 'text-red-500' : 'text-gray-900'} 
+              dark:${errorMessage ? 'text-red-400' : 'text-white'}`}>
+              {errorMessage ? errorMessage : 'Accede con tus credenciales'}
+
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo</label>
-                  <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"  />
+                  <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"  />
                 </div>
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  />
+                  <input type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  />
                 </div>
 
                 <button type="submit" className="w-full text-white hover:text-black bg-black hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-black dark:hover:bg-yellow-400 dark:focus:ring-black">Acceder</button>
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get('erp_token');
+
+  if (!token) {
+    // Redirige al usuario a la página de inicio si no está autenticado
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  // Si el token existe, continúa con la solicitud
+  return NextResponse.next();
+}
+
+// Configura las rutas que deben usar este middleware
+export const config = {
+  matcher: ['/dashboard/:path*'], // Agrega todas las rutas protegidas aquí
+};
