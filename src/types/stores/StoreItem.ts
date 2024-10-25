0751417cfd4e0545dda8e3a6839076bc1e8a288d
brunfrag <brfraga99@gmail.com>
2024-10-25 15:29:06 +0200

    feat(): interfaces folder

---
+++ src/types/stores/StoreItem.ts
@@ -0,0 +1,23 @@
+interface Supplier{
+    Name:string;
+    DeliveryTime:number;
+  }
+  
+  interface SupplierItem{
+    SupplierSku:string;
+    Supplier:Supplier;
+  }
+  
+  interface ItemData{
+    ID:number
+    Name:string;
+    SupplierItems:SupplierItem[];
+  }
+  
+   export default interface StoreItems {
+    Itemname: string;
+    SKU: string;
+    Amount: string;
+    Item: ItemData;
+    SKU_Parent:string;
+  }
---
+++ src/types/suppliers/Supplier.ts
@@ -0,0 +1,8 @@
+ interface Supplier {
+    Id:number,
+    Name:string;
+  }
+
+  export default interface Suppliers{
+    Data: Supplier[];
+}
