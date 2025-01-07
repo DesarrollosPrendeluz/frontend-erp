825fa47f4e6c4d357554a897a6e2662e6a263654
bruno <brunofraga@prendeluz.com>
2025-01-07 16:22:25 +0100

    feat: implement CSV export functionality using API for order data retrieval

--- src/app/dashboard/stagging/[orderCode]/page.tsx
+++ src/app/dashboard/stagging/[orderCode]/page.tsx
@@ -119,46 +119,78 @@ const Stagging = ({ params }: { params: { orderCode: string } }) => {
     });
   };
   const onExportCSV = () => {
-    if (!order || !order.Lines) return;
+    // if (!order || !order.Lines) return;
 
-    const csvData = order.Lines.flatMap((line) => {
-      const linePalletsAndBoxes = palletsAndBoxes[line.id];
-      const orderCode = order.FatherOrder.Childs.find((child) => child.id === line.order_id)?.code
+    // const csvData = order.Lines.flatMap((line) => {
+    //   const linePalletsAndBoxes = palletsAndBoxes[line.id];
+    //   const orderCode = order.FatherOrder.Childs.find((child) => child.id === line.order_id)?.code
 
-      if (!linePalletsAndBoxes) {
-        return [];
+    //   if (!linePalletsAndBoxes) {
+    //     return [];
+    //   }
+
+    //   const pallets = linePalletsAndBoxes.pallet.split(",").map((p) => p.trim());
+    //   const boxes = linePalletsAndBoxes.box.split(",").map((b) => b.trim());
+
+    //   return boxes.map((box) => ({
+    //     PO: orderCode,
+    //     EAN: line.ean,
+    //     Pallet: pallets.join(", "),
+    //     Box: box,
+    //     Cantidad: line.recived_quantity
+    //   }));
+    // });
+
+    // const csvContent = [
+    //   ["PO", "EAN", "Pallet", "Box", "Cantidad"],
+    //   ...csvData.map((row) =>
+    //     [row.PO, row.EAN, row.Pallet, row.Box, row.Cantidad].join(",")
+    //   ),
+    // ]
+    //   .join("\n")
+    //   .replace(/(^\[|\]$)/gm, "");
+
+    // const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+    // const url = URL.createObjectURL(blob);
+    // const link = document.createElement("a");
+    // link.href = url;
+    // link.setAttribute("download", `order_${order.FatherOrder.code}.csv`);
+    // document.body.appendChild(link);
+    // link.click();
+    // document.body.removeChild(link);
+    const token = Cookies.get("erp_token");
+    const apiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL as string;
+    axios.get(apiUrl + "/fatherOrder/amazonExcel?fatherOrderId="+order?.FatherOrder.id,{
+      headers: {
+        Authorization: `Bearer ${token}`
+      }}).then((response2) => {
+       
+        const fileName = response2.data.Results.filename; // Nombre del archivo
+        const fileContent = response2.data.Results.file; // Contenido del archivo (en base64 o texto)
+
+        // Convertir el contenido si es base64
+        const binaryContent = atob(fileContent); // Decodificar base64 a binario
+        const byteNumbers = new Uint8Array(binaryContent.length);
+        for (let i = 0; i < binaryContent.length; i++) {
+          byteNumbers[i] = binaryContent.charCodeAt(i);
+        }
+
+        const blob = new Blob([byteNumbers], { type: 'application/octet-stream' });
+        const url = window.URL.createObjectURL(blob);
+
+        // Crear y simular clic en el enlace
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName; // Asignar el nombre del archivo
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+
+        // Revocar la URL para liberar memoria
+        window.URL.revokeObjectURL(url);
+      
+        });
       }
-
-      const pallets = linePalletsAndBoxes.pallet.split(",").map((p) => p.trim());
-      const boxes = linePalletsAndBoxes.box.split(",").map((b) => b.trim());
-
-      return boxes.map((box) => ({
-        PO: orderCode,
-        EAN: line.ean,
-        Pallet: pallets.join(", "),
-        Box: box,
-        Cantidad: line.recived_quantity
-      }));
-    });
-
-    const csvContent = [
-      ["PO", "EAN", "Pallet", "Box", "Cantidad"],
-      ...csvData.map((row) =>
-        [row.PO, row.EAN, row.Pallet, row.Box, row.Cantidad].join(",")
-      ),
-    ]
-      .join("\n")
-      .replace(/(^\[|\]$)/gm, "");
-
-    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.setAttribute("download", `order_${order.FatherOrder.code}.csv`);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
 
 
 
